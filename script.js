be29bae634629e8febbff34452554be4e7b62bd6
iyinoluwAA <iyinoluwa150@gmail.com>
2024-03-27 08:49:29 +0100

    add some updates like object, date and time. schedule application pro. pro max coming up

--- script.js
+++ script.js
@@ -4,30 +4,42 @@
 
 
    function submitBtn() {
-    var inputTag = document.getElementById('Todo').value
-    if (inputTag === ''){
-      err.style.display = 'block'
-      
-    } else {
-      err.style.display = 'none'
+     if ((Todo.value && TodoTwo.value  )  === ''){
+       err.style.display = 'block'
+       
+      } else {
+
+        var todoContainer = {
+           inputTag : document.getElementById('Todo').value,
+           inputTagTwo : document.getElementById('TodoTwo').value
+        }
+
+        err.style.display = 'none'
       errnoItem.style.display = 'none'
       noItem.style.display = 'none'
-     todoLIst.push(inputTag)
+     todoLIst.push(todoContainer)
       console.log(todoLIst);
-     displayITem()
-     document.getElementById("Todo").value =''
+      document.getElementById("Todo").value =''
+      document.getElementById("TodoTwo").value = ''
     }
+    displayITem()
    }
    function displayITem(){
+    newTodo.value = Todo.value
+    newTodoTwo.value = TodoTwo.value
     show.innerHTML = '';  
     
     todoLIst.map(
-        (items,number)=>{
+        (todoContainer,number)=>{
+          var time = new Date().toLocaleTimeString()
+          var date = new Date().toLocaleDateString()
+  
         show.innerHTML += `
         <div class="card" ">
        <div class="card-body">
        <h5 class="card-title"> ${number + 1}</h5>
-       <p class="card-text">${items} </p>
+       <p class="card-text">${todoContainer.inputTag} </p>
+       <p class="card-text">${todoContainer.inputTagTwo}<br> ${date} ${time}</p>
        <a href="#" class="btn btn-warning"  data-bs-toggle="modal" data-bs-target="#exampleModal"><i class="fa-solid fa-pen-to-square" onclick="edit(${number})"></i></a>
        <a href="#" class="btn btn-danger"  onclick="delTodo(${number})"><i class="fa-solid fa-trash"></i></a>
        </div>
@@ -49,11 +61,14 @@
       newNumber = i
    }
    function editItem() {
-    if (newTodo.value == "") {
+    if ((newTodo.value && newTodoTwo.value) == "") {
         alert("No way")
     } else {
-        var newValue = document.getElementById('newTodo').value
-        todoLIst.splice(newNumber, 1, newValue)
+       var modalAll = {
+        inputTag: document.getElementById("newTodo").value,
+        inputTagTwo: document.getElementById("newTodoTwo").value
+       }
+        todoLIst.splice(newNumber, 1 , modalAll)
        displayITem()
     }
 }
